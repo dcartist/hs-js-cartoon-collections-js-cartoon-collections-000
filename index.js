06c9c6b1eda1491767a744dba4ebc89ebdadbe8f
dcartist <pbannerman13@gmail.com>
2018-07-10 12:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*Adding in comments now so you can understand my logic better
 than attempting to speak especially since i'm already nervous.
 Warning my logic isn't fully considered as normal, but it gets things done */
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves) { 
   var count = 1 //the starting count for the dwarves
   var returnDwarves = "" //to hold the final string value in the dwarves from the array
   //this for loop for through the dwarves and inserts them into
@@ -29,13 +29,15 @@ function longPlaneteerCalls(words) {
  return wordResults //final results returned
 }
 
+findTheCheese(["apple","gouda", "cheddar", "banana"])
+
 function findTheCheese (foods) {
   /*was having issues with the test, due to software update errors
   and the test would freeze completely without giving me a full
   reason what was wrong on the test...
   So I went overboard but this does work
   don't judge me because I go overboard with cautions */
-  var cheeseFinder = ["cheddar","gouda","camembert"]//array of the cheeses that was
+  var cheeseFinder = ["cheddar","gouda","camembert", "Swiss"]//array of the cheeses that was
   var noReturn = true //default setting if there is no cheese is found
   var theCheese = ""
   /* theCheese is an empty string to add in the cheese if it was found.
@@ -57,4 +59,52 @@ function findTheCheese (foods) {
   } else { //else if the default was set to false instead of true it will return the name of the cheese
     return theCheese
   }//end of if condition
+}
+
+function wordsWithB(words) {
+  var bwords = []
+  
+}
+
+wordsWithB(["paula", "rishi", "bill"]) // ["bill"]
+wordsWithB(["bob", "brenda", "carolyn"]) // ["bob", "brenda"]
+
+function summonCaptainPlanet(planeteerCalls){
+  for(var i=0; i<planeteerCalls.length; i++){
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!"
+  }
+  return planeteerCalls
+}
+
+function longPlaneteerCalls(words) {
+  wordResults = false
+  for(var i=0; i<words.length; i++){
+  if (words[i].length>4) {
+    wordResults = true
+  }//end of if loop
+}//end of for loops
+ return wordResults
+}
+
+function findTheCheese (foods) {
+  var cheeseFinder = ["cheddar","gouda","camembert"]
+  var noReturn = true
+  var theCheese = ""
+  var noCheese = 'no cheese!'
+  for(var i=0;i<foods.length;i++){ //Food Loop
+
+    for (var x=0;x<cheeseFinder.length;x++){//Cheese Loop
+      while (cheeseFinder[x] == foods[i]){
+        noReturn = false
+        theCheese = cheeseFinder[x]
+        return theCheese
+      }//End while loops
+    }//End of cheese loop
+  }//end of food loop
+  if(noReturn == true){
+    return noCheese
+  } else {
+    return theCheese
+  }
+>>>>>>> d2b78b045f3412f60ed43e3adda7ab7499673f65
 }
